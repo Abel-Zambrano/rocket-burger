88535c892a139c5a673de3cf79de66ca4e42525e
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-03 18:30:02 -0800

    added components

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -5,7 +5,7 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
     // Transform object to array
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
@@ -14,7 +14,9 @@ const Burger = (props) => {
         .reduce((arr, el) => {
             return arr.concat(el)
         }, []);
-    console.log(transformedIngredients);
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add ingredients!</p>
+    }
     
 
     return (
--- src/containers/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder.jsx
@@ -1,24 +1,21 @@
 import React, { useState } from 'react';
 import Burger from '../components/Burger/Burger';
+import BuildControls from '../components/Burger/BuildControls/BuildControls';
 
 const BurgerBuilder = () => {
     const [ ingredients, setIngredients ] = useState({
-        lettuce: 1,
+        lettuce: 0,
         bacon: 0,
-        cheese: 4,
-        meat: 1
+        cheese: 0,
+        meat: 0
     });
 
     return (
         <>
             <Burger ingredients={ingredients} />
-            <div>Build Controls</div>
+            <BuildControls />
         </>
     );
 }
-
-
-    
-    
 
 export default BurgerBuilder;
