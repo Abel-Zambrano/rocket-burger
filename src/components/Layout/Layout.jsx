a8e23e2d3db6982e2b0b547aaed05f56797f022a
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-07 11:48:27 -0800

    update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
+const Layout = (props) => {
+    const [showSideDrawer, setShowSideDrawer] = useState(true);
+
+    const sideDrawerHandler = () => {
+        setShowSideDrawer(false);
+    };
+
+    return (
         <>
             <Toolbar />
-            <SideDrawer />
+            <SideDrawer open={showSideDrawer} closed={sideDrawerHandler} />
             <main className="content">{props.children}</main>            
         </>
-    );
+    )};
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -3,18 +3,29 @@ import Logo from '../../Logo/Logo';
 import Title from '../../Title/Title';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const SideDrawer = (props) => {
+    let assignedClasses = ['SideDrawer', 'Closed'];
+    if (props.open) {
+        assignedClasses = ['SideDrawer', 'Open'];
+    };
+   
     return (
-        <div className='SideDrawer'>
-            <div className='SideDrawerBrand'>
-                <Logo />
-                <Title />
+        <>
+            <Backdrop clicked={props.closed} show={props.open}  />
+            {console.log(props.closed)
+            }
+            <div className={assignedClasses.join(' ')}>
+                <div className='SideDrawerBrand'>
+                    <Logo />
+                    <Title />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </>
     );
 };
 
