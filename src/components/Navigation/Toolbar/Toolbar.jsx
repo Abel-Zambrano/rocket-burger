e4e50d099c6c81cf10dd6da957d2ab286b5e67bb
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-06 20:37:37 -0800

    Added Navigation

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const Toolbar = () => {
     return (
         <header className='Toolbar'>
             <div>Menu</div>
             <Logo />
-            <nav>
-                ...
-            </nav>
+            <NavigationItems />
         </header>
     );
 };
