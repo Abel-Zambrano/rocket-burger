5cb1cc830ee15d853551e956b322ab514e066fc1
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-09 19:32:54 -0800

    added error handler

---
+++ src/hoc/withErrorHandler/withErrorHandler.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent) => {
+    return (props) => {
+        return (
+            <>
+                <Modal>
+                    Something went wrong...
+                </Modal>
+                <WrappedComponent {...props} />
+            </>
+        );
+    };
+};
+
+export default withErrorHandler;
