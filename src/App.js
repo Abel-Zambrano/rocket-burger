1d8cd08cc609233f0cd61f5409b1604d23b7b857
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-12-14 18:11:59 -0800

    added Switch and Route

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path='/checkout' component={Checkout} />
+            <Route path='/' component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     </BrowserRouter>
